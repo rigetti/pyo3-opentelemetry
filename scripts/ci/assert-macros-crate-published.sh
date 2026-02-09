4ec2a42c1bd242854183c5255f074b6c064edc86
Alexander Saites <215926835+asaites@users.noreply.github.com>
2026-02-09 11:43:34 -0700

    ci: make sure we capture the grep status

--- scripts/ci/assert-macros-crate-published.sh
+++ scripts/ci/assert-macros-crate-published.sh
@@ -4,16 +4,14 @@
 # It is used in the CI pipeline to ensure that the version of the macros crate is published
 # before publishing the version of the lib crate that depends on it.
 
-set -e
-
 CRATE="pyo3-opentelemetry-macros"
 
-if cargo publish --dry-run -p "$CRATE" 2> >(tee /dev/stderr | \
-  grep -Eq "warning: crate ${CRATE}@.+ already exists"); then
-  echo "Current version of $CRATE is not yet published."
-  exit 1
+if cargo publish --dry-run -p "$CRATE" 2>&1 | tee /dev/stderr \
+  | grep -Eq "warning: crate ${CRATE}@.+ already exists"; then
+  echo "Current version of $CRATE is published."
+  exit 0
 fi
 
-echo "Current version of $CRATE is published."
-exit 0
+echo "Current version of $CRATE is not yet published."
+exit 1
 
